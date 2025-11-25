55c77641f1fcc1b82f17a770092d6eae22bfe654
the-jekey <jairamparihar79@gmail.com>
2025-11-25 22:03:51 +0530

    first commit

---
+++ src/Comonents/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+       return (
+              <div className='h-screen flex justify-center items-center bg-gray-900 text-white '>
+
+              </div>
+       )
+}
+
+export default Home
