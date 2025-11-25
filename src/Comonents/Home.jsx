03d2534e9d25dcc7dedd8c8cca9a5b1ba9dd9be0
the-jekey <jairamparihar79@gmail.com>
2025-11-25 22:06:46 +0530

    Navbar added

--- src/Comonents/Home.jsx
+++ src/Comonents/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Home = () => {
        return (
               <div className='h-screen flex justify-center items-center bg-gray-900 text-white '>
-
+                     <div className="">
+                            <Navbar />
+                     </div>
               </div>
        )
 }
